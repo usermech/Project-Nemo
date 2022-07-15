52b162665b0f050080082a97ba08b0ed84a99f57
usermech <96394892+usermech@users.noreply.github.com>
2022-07-15 16:55:20 +0300

    Add files via upload

---
+++ nemo_ws/src/build/apriltag_ros/colcon_command_prefix_build.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/ubuntu/nemo_ws/src/install/cv_bridge/share/cv_bridge/package.sh"
+. "/home/ubuntu/nemo_ws/src/install/image_transport/share/image_transport/package.sh"
