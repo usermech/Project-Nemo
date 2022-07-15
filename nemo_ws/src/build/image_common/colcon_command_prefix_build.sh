82a717dd76c944be0a5be33115206963196d70b6
usermech <96394892+usermech@users.noreply.github.com>
2022-07-15 17:14:56 +0300

    Add files via upload

---
+++ nemo_ws/src/build/image_common/colcon_command_prefix_build.sh
@@ -0,0 +1,4 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/ubuntu/nemo_ws/src/install/camera_calibration_parsers/share/camera_calibration_parsers/package.sh"
+. "/home/ubuntu/nemo_ws/src/install/image_transport/share/image_transport/package.sh"
+. "/home/ubuntu/nemo_ws/src/install/camera_info_manager/share/camera_info_manager/package.sh"
