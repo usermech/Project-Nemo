358c31be061f761eca5269cef3d7d173d1278699
usermech <96394892+usermech@users.noreply.github.com>
2022-07-15 17:13:47 +0300

    Add files via upload

---
+++ nemo_ws/src/build/cv_bridge/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/nemo_ws/src/vision_opencv/cv_bridge
+# Build directory: /home/ubuntu/nemo_ws/src/build/cv_bridge
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("python")
+subdirs("src")
+subdirs("test")
